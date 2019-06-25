0a667c489a16c38302b9adf1b1b91f9d03c1141e
Alejandro Peinó <alepeino@gmail.com>
2019-06-25 09:45:40 -0300

    Don't align map values

--- src/dmic/db/connection.clj
+++ src/dmic/db/connection.clj
@@ -3,9 +3,9 @@
             [dotenv :refer [env]]))
 
 (def cfg {:server-type :peer-server
-          :access-key  (env :ACCESS_KEY)
-          :secret      (env :SECRET)
-          :endpoint    (str (env :PEER_SERVER_HOST) ":" (env :PEER_SERVER_PORT))})
+          :access-key (env :ACCESS_KEY)
+          :secret (env :SECRET)
+          :endpoint (str (env :PEER_SERVER_HOST) ":" (env :PEER_SERVER_PORT))})
 
 (def client (d/client cfg))
 
