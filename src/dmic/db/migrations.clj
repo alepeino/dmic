472e56a18a3ca976db6d2a0a9b79543bd3bf8e44
Alejandro Peinó <alepeino@gmail.com>
2019-07-01 17:48:01 -0300

    WIP Migrations

--- src/dmic/db/migrations.clj
+++ src/dmic/db/migrations.clj
@@ -10,72 +10,72 @@
 (def types ["provincia" "departamento"])
 
 (defn type->keyword [type]
-  (-> type (s/replace " " "-") keyword))
+  (-> type name (s/replace " " "-") keyword))
 
-(prn
-  "Initializing Datofu"
-  (d/transact conn {:tx-data (array/schema-tx)}))
-
-(prn
-  "Creating schema"
-  (d/transact conn {:tx-data schema/node-schema}))
-
-;(doseq [[n type] (take 1 (map-indexed vector types))]
-
-
-(defn make-node [ancestors-map {:keys [name type parent]}]
-  (let [parent-id (-> parent :name ancestors-map)]
-    (cond-> {:node/name name
-             :node/type (type->keyword type)}
-      parent-id (assoc :node/parent parent-id
-                       :node/ancestors [{:datofu.coll.array.cell/index 0
-                                         :datofu.coll.array.cell/ref parent-id}]))))
+(defn node-additions [ancestors-map {:keys [name type ancestors]}]
+  (let [ancestor-names (for [n (range (count ancestors))]
+                         (->> (take (inc n) ancestors)
+                           (map :name)
+                           (interpose "-")
+                           (apply str)))
+        temp-id (->> [name]
+                  (concat ancestor-names)
+                  (interpose "-")
+                  (apply str))
+        ancestor-ids (keep ancestors-map ancestor-names)]
+        ;ancestor-id (prn parent-id ancestors-map)]))
+    (concat [[:db/add temp-id :node/name name]
+             [:db/add temp-id :node/type type]]
+            (vec (map-indexed #(vector %1 %2) ancestor-ids)))))
+    ;(cond->> [{:db/add :node/name name
+    ;           :node/type (type->keyword type)]
+    ;  parent-id (assoc :node/parent parent-id
+    ;                   :node/ancestors [{:datofu.coll.array.cell/index 0
+    ;                                     :datofu.coll.array.cell/ref parent-id)]))
 
 (defn by-type [type]
   (let [reader (clojure.java.io/reader "data.json")]
     (->> (cheshire/parse-stream reader true)
       (filter (comp #{type} :type)))))
 
-(defn find-by-type [type]
-  (let [q (d/q '[:find ?e ?n
-                 :in $ ?type
-                 :where [?e :node/type ?type]
-                 [?e :node/name ?n]]
-            (d/db conn)
-            (type->keyword type))]
-    (->> q
-      (map (comp vec reverse))
-      (into {}))))
+;(defn find-by-type [type]
+;  (let [q (d/q '[:find ?e ?n
+;                 :in $ ?type
+;                 :where [?e :node/type ?type]
+;                        [?e :node/name ?n]
+;                        [?e :node/ancestors ?an-cel]]
+;            (d/db conn)
+;            (type->keyword type)
+;            '[])]
+;    (->> q
+;      (map (comp vec reverse))
+;      (into {}))))
 
 (defn migrate-node-by-type [[type & rest] ancestors-map]
-  (prn
-    (str "Seeding type " type)
-    (->>
-      (by-type type)
-      (map (partial make-node ancestors-map))
-      (hash-map :tx-data)
-      (d/transact conn)))
-  (when (seq rest)
-    (migrate-node-by-type
-      rest
-      (merge ancestors-map (find-by-type type)))))
+  (let [parsed (by-type type)]
+    (->> parsed
+      (map (partial node-additions ancestors-map))
+      prn)))
+    ;(prn
+    ;  (str "Seeding type " type)
+    ;  (->> parsed
+    ;    (map (partial make-node ancestors-map))
+    ;    (hash-map :tx-data)
+    ;    (d/transact conn)]
+    ;(->> parsed
+    ;  (map :parent)
+    ;  (prn "XXXXXX")
+    ;(when (seq rest)
+    ;  (migrate-node-by-type
+    ;    rest
+    ;    (merge ancestors-map (find-by-type type))]))
 
-;(->> (find-by-type "provincia")
-;  prn)
-
-;(let [q (d/q '[:find ?e ?n (sum ?a)
-;               :where [?e :node/type]
-;                      [?e :node/name ?n]
-;                      (or-join [?e ?a]
-;                        (and
-;                          [(ground 0) ?a])
-;                        (and
-;                          [?e :node/ancestors ?ancestor]
-;                          [(ground 1) ?a]))]
-;          (d/db conn))]
-;  (->> q
-;    (map (fn [[_ _ n]] n))
-;    (frequencies)
-;    (prn)))
-;(migrate-node-by-type types {})
-;(d/transact conn {:tx-data [{:db/excise 17592186046801}]})
+;(prn
+;  "Initializing Datofu"
+;  (d/transact conn {:tx-data (array/schema-tx)}))
+;
+;(prn
+;  "Creating schema"
+;  (d/transact conn {:tx-data schema/node-schema}))
+;
+(migrate-node-by-type (rest types) {})
--- src/dmic/db/schema.clj
+++ src/dmic/db/schema.clj
@@ -1,33 +1,10 @@
-(ns dmic.db.schema)
+(ns dmic.db.schema
+  (:require [datofu.schema.dsl :as s]))
 
 (def node-schema
-  [{:db/ident :node/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Node name"}
-
-   {:db/ident :node/type
-    :db/valueType :db.type/keyword
-    :db/cardinality :db.cardinality/one
-    :db/doc "Node type"}
-
-   {:db/ident :node/id
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Custom ID for a node"}
-
-   {:db/ident :node/parent
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "Node parent"}
-
-   {:db/ident :node/ancestors
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    ;:db/isComponent true
-    :db/doc "Node ancestors"}
-
-   {:db/ident :node/owners
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Node owners"}])
+  [(s/attr :node/name :string "Node name")
+   (s/attr :node/type :keyword "Node type")
+   (s/attr :node/id :string "Custom ID for a node")
+   (s/to-one :node/parent "Node parent")
+   (s/to-many :node/ancestors :component "Node ancestors")
+   (s/to-many :node/owners "Node ancestors")])
